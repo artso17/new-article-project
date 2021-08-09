1b30e34b4ce19564f8e170b1057d5dc84d5367d1
Aditya <defuse805@gmail.com>
2021-08-09 19:26:32 +0700

    akhir dari artsodeva.1 kedua

--- website/website/settings.py
+++ website/website/settings.py
@@ -200,27 +200,27 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # EMAIL_USE_SSL = config('EMAIL_USE_SSL', cast=bool)
 
 # http settings
-# SESSION_COOKIE_SECURE = config(
-#     'SESSION_COOKIE_SECURE', default=False, cast=bool)
-# CSRF_COOKIE_SECURE = config('CSRF_COOKIE_SECURE', default=False, cast=bool)
-# SECURE_SSL_REDIRECT = config('SECURE_SSL_REDIRECT', default=False, cast=bool)
+SESSION_COOKIE_SECURE = config(
+    'SESSION_COOKIE_SECURE', default=False, cast=bool)
+CSRF_COOKIE_SECURE = config('CSRF_COOKIE_SECURE', default=False, cast=bool)
+SECURE_SSL_REDIRECT = config('SECURE_SSL_REDIRECT', default=False, cast=bool)
 
 
 # hsts settings
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = config(
-#     'SECURE_HSTS_INCLUDE_SUBDOMAINS', default=False, cast=bool)
-# SECURE_HSTS_PRELOAD = config('SECURE_HSTS_PRELOAD', default=False, cast=bool)
-# SECURE_HSTS_SECONDS = config('SECURE_HSTS_SECONDS', default=False, cast=int)
+SECURE_HSTS_INCLUDE_SUBDOMAINS = config(
+    'SECURE_HSTS_INCLUDE_SUBDOMAINS', default=False, cast=bool)
+SECURE_HSTS_PRELOAD = config('SECURE_HSTS_PRELOAD', default=False, cast=bool)
+SECURE_HSTS_SECONDS = config('SECURE_HSTS_SECONDS', default=False, cast=int)
 print(ALLOWED_HOSTS)
 
 # secure policy
-# SECURE_REFERRER_POLICY = config('SECURE_REFERRER_POLICY')
+SECURE_REFERRER_POLICY = config('SECURE_REFERRER_POLICY')
 
 
 # account settings
-# LOGIN_REDIRECT_URL = '/admin-list/'
-# ACCOUNT_AUTHENTICATED_LOGIN_REDIRECTS = True
-# ACCOUNT_EMAIL_REQUIRED = True
-# ACCOUNT_EMAIL_VERIFICATION = "optional"
-# ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = True
-# ACCOUNT_LOGIN_ON_PASSWORD_RESET = True
+LOGIN_REDIRECT_URL = '/admin-list/'
+ACCOUNT_AUTHENTICATED_LOGIN_REDIRECTS = True
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = "optional"
+ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = True
+ACCOUNT_LOGIN_ON_PASSWORD_RESET = True
