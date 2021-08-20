a83d2d03704d78edfe60ea6226e2c5949b1981c4
Aditya <defuse805@gmail.com>
2021-08-20 21:34:01 +0700

    update.1.5.9

--- website/static/js/article_detail.js
+++ website/static/js/article_detail.js
@@ -5,16 +5,11 @@ const csrf=document.getElementsByName('csrfmiddlewaretoken')[0].value
 const numLikes=document.getElementsByClassName('num-likes')[0]
 const commentInput=document.getElementById('comment')
 const commentCont=document.getElementsByClassName('comment-container')[0]
-const title=encodeURIComponent(document.getElementsByTagName('h2')[0].innerHTML)
-const socialShare=document.getElementsByClassName('social-share')
-const posttitle=encodeURIComponent(`${title} Ingin tahu kelanjutannya? Cek selengkapnya di Artsodevalog: `)
-const postUrl=encodeURIComponent(window.location.href)
+
 // console.log(posttitle)
 import * as modul from './module.js'
 
-socialShare['facebook_share'].setAttribute('href',`https://www.facebook.com/sharer.php?u=${postUrl}`)
-socialShare['whatsapp_share'].setAttribute('href',`https://api.whatsapp.com/send?text=${posttitle}${postUrl}`)
-socialShare['linkedin_share'].setAttribute('href',`https://www.linkedin.com/shareArticle?mini=true&url=${postUrl}&title=${posttitle}`)
+
 if (btn['show_more']!= undefined) btn['show_more'].addEventListener('click',e=>modul.showMoreComm(csrf,pk,e.target,commentCont,e.target.parentNode,btn['show_less'].parentNode))
 
 btn['show_less'].addEventListener('click',e=>{
