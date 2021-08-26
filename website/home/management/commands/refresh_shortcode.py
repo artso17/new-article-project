4a3021c12a38b626c77b470555730914ad76d35e
Aditya Prasetyo Budiarso <defuse805@gmail.com>
2021-08-26 19:43:04 +0700

    update.1.5.24.2 menambahkan commands refresh_shortcode

---
+++ website/home/management/commands/refresh_shortcode.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand, CommandError
+from home.models import Article
+from home.utils import check_code
+
+
+class Command(BaseCommand):
+    help = 'Closes the specified poll for voting'
+
+    def handle(self, *args, **options):
+        articles = Article.objects.all()
+        old_codes = articles.count()
+        new_codes = 0
+        for article in articles:
+            article.shortcode = check_code(article)
+            new_codes += 1
+            print(article.shortcode)
+        return f'Success generate {new_codes} from {old_codes} codes'
